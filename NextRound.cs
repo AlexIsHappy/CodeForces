8054964c2240c5cf50a6902142146749c4920e1e
AlexIsHappy <alexishappy@yandex.ru>
2015-03-10 13:13:31 +0300

    First Solutions

---
+++ NextRound.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            String[] words = new String[n];
+            int[] wordLength = new int [n];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = Console.ReadLine();
+                wordLength[i] = words[i].Length;
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (wordLength[i] <= 10)
+                    Console.WriteLine(words[i]);
+                else
+                    Console.WriteLine(words[i][0] + (words[i].Length - 2).ToString() + words[i][words.Length -1]);
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
