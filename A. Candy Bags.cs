d8653b1c5fae197430dcffc55f3c15aa82bcbc40
AlexIsHappy <alexishappy@yandex.ru>
2015-05-15 14:46:33 +0200

    5/15/2015

---
+++ A. Candy Bags.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+         
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = Sum(num);
+
+
+            List<int> array = GenerateArray(num * num);
+
+            List<List<int>> arrays = new List<List<int>>();
+
+
+               for (int i = 0; i < num; i++)
+                {
+                    List<int> l = new List<int>();
+                    while (l.Count < num) 
+                    {
+                        l.Add(array[0]);
+                        l.Add(array[array.Count - 1]);
+                        array.RemoveAt(0);
+                        array.RemoveAt(array.Count - 1);
+                    }
+                    arrays.Add(l);
+                }
+
+            foreach(List<int> l in arrays)
+            {
+                foreach(int i in l)
+                {
+                    Console.Write(i + " ");
+                } 
+                Console.WriteLine();
+            }
+
+                Console.ReadLine();
+        }
+
+        public static int Sum(int n)
+        {
+            int res = 0;
+            int num = n * n;
+            while (num > 0)
+            {
+                res += num;
+                num--;
+            }
+            return res;  
+        }
+
+        public static List<int> GenerateArray(int n)
+        {
+            List<int> array = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                array.Add(i + 1);
+            }
+
+            return array;
+        }
+    } 
+}
---
+++ A. The number of positions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] b = new int[] { 121, 14641, 20736, 36100, 25921, 361, 20736, 361 };
+            bool r = comp(a, b); // True
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+
+        public static bool comp(int[] a, int [] b)
+        {
+            if ((a.Length == 0 && b.Length == 0))
+                return true;
+            if (a.Length == 0 || b.Length == 0 || a == null || b == null)
+                return false;
+            if (a.Length != b.Length)
+                return false;
+            int countT = 0;
+            foreach (int i in b)
+            {
+                bool check = false;
+                foreach(int j in a)
+                {
+                    if (Math.Sqrt(i) == j)
+                    {
+                        check = true;
+                        countT++;
+                        break;
+                    }
+                }
+
+                if (check == false)
+                    return false;
+            }
+
+            Console.WriteLine(b.Length + "  " + countT);
+            return countT == b.Length ? true : false;
+        }
+    }
+}
